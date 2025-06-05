5dacdbab3fc54f7702fe06995e1ef309ff877294
Asfar Javed <asfarali7172@gmail.com>
2025-06-05 09:34:49 +0500

    successfully deleted

--- Meal-App-Backend/controllers/mealController.js
+++ Meal-App-Backend/controllers/mealController.js
@@ -1,5 +1,51 @@
 import Meal from '../models/mealModel.js';
 
+export const deleteCategoryMeals = async (req, res) => {
+  try {
+    const categoryTitle = req.params.category;
+
+    const result = await Meal.deleteMany({ category: categoryTitle });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: 'No meals found in this category',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Deleted all meals in category: ${categoryTitle}`,
+    });
+  } catch (error) {
+    console.error('Error deleting category meals:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Server error',
+    });
+  }
+};
+
+
+
+export const getAllMeals = async (req, res) => {
+  try {
+    const meals = await Meal.find({}).sort({ createdAt: -1 }); // Sort by newest first
+    
+    res.status(200).json({
+      success: true,
+      count: meals.length,
+      data: meals,
+    });
+  } catch (error) {
+    console.error('Error fetching meals:', error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 export const addMeal = async (req, res) => {
   try {
     const { title, category, duration, imageUrl, ingredients, steps } = req.body;
--- Meal-App-Backend/routes/mealRoutes.js
+++ Meal-App-Backend/routes/mealRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { addMeal } from '../controllers/mealController.js';
+import { addMeal, getAllMeals, deleteCategoryMeals } from '../controllers/mealController.js';
 
 const router = express.Router();
 
 router.post('/meals', addMeal);
+router.get('/getmeals', getAllMeals);
+
+router.delete('/deletecategory/:category', deleteCategoryMeals);
+
 
 export default router;
